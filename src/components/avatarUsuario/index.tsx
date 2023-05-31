6111c58d703bf287791f5157bfdb34ac818e94ed
serafim <matheus18serafim@gmail.com>
2023-05-31 12:24:20 -0300

    updated/authentications app 2

--- src/components/avatarUsuario/index.tsx
+++ src/components/avatarUsuario/index.tsx
@@ -6,7 +6,7 @@ export default function AvatarUsuario() {
   return (
     <Link href={"/perfil"} className="w-10 h-10 ml-4">
       <img
-        src={usuario?.imagemUrl ?? "/semusuario.png"}
+        src={"/semusuario.png"}
         alt="Avatar do usuário"
         className="rounded-full border-2 border-gray-950 object-contain cursor-pointer"
       />
