fe1ccbce63f575305e1b72d48701d2e4c487d037
serafim <matheus18serafim@gmail.com>
2023-05-31 14:36:07 -0300

    updated/axios default headers

--- src/API-PURO/index.ts
+++ src/API-PURO/index.ts
@@ -1,5 +1,12 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
 export const apiPuro = axios.create({
   baseURL: "https://hammerhead-app-ahoua.ondigitalocean.app",
 });
+
+if (Cookies.get("admin-puro-auth-token")) {
+  const token: any = Cookies.get("admin-puro-auth-token");
+
+  apiPuro.defaults.headers["Authorization"] = `Bearer ${token}`;
+}
